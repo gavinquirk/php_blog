c2d7145973cd3bb2324cc73c80d07992c86fa6af
Gavin Quirk <gavinquirk@gmail.com>
2019-08-28 17:00:14 -0700

    delete post

--- models/Post.php
+++ models/Post.php
@@ -162,5 +162,30 @@
       return false;
     }
 
+    // Delete Post
+    public function delete() {
+      // Create query
+      $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Clean Data
+      $this->id = htmlspecialchars(strip_tags($this->id));
+
+      // Bind Data
+      $stmt->bindParam(':id', $this->id);
+
+      // Execute Query
+      if ($stmt->execute()) {
+        return true;
+      }
+
+      // Print error if something goes wrong
+      printf("Error: %s.\n", $stmt->error);
+
+      return false;
+    }
+
 
   }
